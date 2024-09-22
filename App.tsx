96443b80c6b6822e9146fda97355e1f132dcfc5f
Andresouzanl <devals2022@gmail.com>
2024-09-22 10:14:46 -0300

    primeirocomit

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, ScrollView, Text} from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <Text>Frases dia das maes</Text>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+export default App;
